66b5eba9df4fed7109cedb701534c67d83ed3663
THIBAULT.G <144021714+Guithi0777@users.noreply.github.com>
2023-12-21 14:00:00 +0100

    Testing my workflow

---
+++ launchingjob1.py
@@ -0,0 +1,8 @@
+mot_de_passe = ""
+while not mot_de_passe == "abcd":   
+          mot_de_passe = input ("Quel est le mot de passe")
+
+if mot_de_passe =='abcd':
+    print (" Mot de passe correct , vous avez accés")
+else: 
+    print (" Mots de passe incorrecte veuillez ressayez") 
